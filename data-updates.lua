5d66014e59eff7f2f4e24dfb4e513ad3de6536d5
snouz <snooz-x@hotmail.com>
2026-07-02 00:14:35 +0200

    Update to 2.1

--- data-updates.lua
+++ data-updates.lua
@@ -1,4 +1,4 @@
 if mods["space-age"] then
-  data.raw.recipe["big-solar-panel"].category = "electronics"
+  data.raw.recipe["big-solar-panel"].categories = {"crafting", "electromagnetics"}
   data.raw.item["big-solar-panel"].default_import_location = "fulgora"
 end
